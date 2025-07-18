e2852cf48518cd17028ba52b65bb97be8b29ca5b
un <un@DESKTOP-JAU235M>
2025-07-18 19:24:09 +0300

    CallingConstructors DONE

--- String/main.cpp
+++ String/main.cpp
@@ -104,7 +104,7 @@ public:
 	//			Methods:
 	void print()const
 	{
-		cout << "Size:\t" << size << endl;
+		cout << "Size:\t\t\t" << size << endl;
 		cout << "Str:\t" << str << endl;
 	}
 };
@@ -128,12 +128,16 @@ void Clear(char* str)
 	delete[] str;
 }
 
-#define CONSTRUCTORS_CHECK
+//#define CONSTRUCTORS_CHECK
 //#define COPY_SEMANTIC_CHECK
 
 void main()
 {
 	setlocale(LC_ALL, "");
+
+	int a = 2;
+	a = 3;
+
 #ifdef CONSTRUCTORS_CHECK
 	String str1;
 	str1.print();
@@ -164,5 +168,43 @@ void main()
 #endif // COPY_SEMANTIC_CHECK
 
 	//MoveMethods (MoveSemantic)
+
+	String str1;	//DefaultConstructor
+	str1.print();
+
+	String str2(5);	//Single-argument Constructor
+	str2.print();
+
+	String str3 = "Hello";	//Single-argument Constructor (const char str[])
+	str3.print();
+
+	String str4();	//В этой строке НЕ вызывает DefaultConstructor, и НЕ создаётся объект
+	//здесь объявляется функция 'str4', которая ничего не принимает,
+	//и возвращает значение типа 'String'
+//str4 НЕ является объектом, это функция с названием 'str4' и возвращаемым значением 'String'
+//ЭТО ПРОТОТИП
+
+//То есть, круглые скобки не делают явный вызов конструктора по умолчанию.
+//Если есть необходимость явно вызвать конструктор по умолчанию
+//то это можно сделать при помощи фигурных скобок
+	String str5{};	//А сэта строка уже яывно вызывает DefaultConstructor()
+	str5.print();
+
+	String str6{ 6 };	//Single-argument Constructor
+	str6.print();
+
+	String str7{ "World" };
+	str7.print();
+
+	String str8 = str7;
+	str8.print();
+
+	String str9(str8);
+	str9.print();
+
+	String str10{ str9 };
+	str10.print();
+
+	//!!! Фигурные скобки для вызова конструктора следует использовать с большой осторожностью
 
 }
