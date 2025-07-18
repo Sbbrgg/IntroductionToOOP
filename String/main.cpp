755561e4ac4ef8acbcea97c4740618abbc5b7764
un <un@DESKTOP-JAU235M>
2025-07-18 18:48:51 +0300

    MoveSemantic DONE

--- String/main.cpp
+++ String/main.cpp
@@ -49,6 +49,17 @@ public:
 			this->str[i] = other.str[i];
 		cout << "CopyConstructor:\t" << this << endl;
 	}
+	String(String&& other)
+	{
+		//MoveConstructor - ShallowCopy:
+		this->size = other.size;
+		this->str = other.str;
+		//Обнуляем принимаемый объект для того, чтобы предотвратить удаление его ресурсов деструктором
+		other.size = 0;
+		other.str = nullptr;
+		cout << "MoveConstructor:\t" << this << endl;
+	}
+
 	~String()
 	{
 		delete[] str;
@@ -65,6 +76,21 @@ public:
 		for (int i = 0; i < size; i++)
 			this->str[i] = other.str[i];
 		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
+	String& operator=(String&& other)
+	{
+		//0) Проверка на не является ли 'this' и 'other' одним и тем же объектом:
+		if (this == &other) return *this;
+		//1) Удаление старой памяти:
+		delete[] str;
+		//2) Shallow copy:
+		this->size = other.size;
+		this->str = other.str;
+		//3) Обнуляем принимаемый объект:
+		other.size = 0;
+		other.str = nullptr;
+		cout << "MoveAssignment:\t\t" << this << endl;
 		return *this;
 	}
 	char operator[](int i) const
@@ -123,7 +149,8 @@ void main()
 	cout << str4 << endl;
 
 	cout << delimiter << endl;
-	String str5 = str3 + str4;
+	String str5;
+	str5 = str3 + str4;
 	cout << delimiter << endl;
 	cout << str5 << endl;
 #endif // CONSTRUCTORS_CHECK
@@ -135,5 +162,7 @@ void main()
 	str2 = str1;
 	cout << str2 << endl;
 #endif // COPY_SEMANTIC_CHECK
+
+	//MoveMethods (MoveSemantic)
 
 }
